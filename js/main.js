1131b0105e774ef3ede31db1e7183f90459123be
Bruno <brunopanzera06@gmail.com>
2023-03-19 14:25:38 +0100

    aggiunto il sito

---
+++ js/main.js
@@ -0,0 +1,85 @@
+function rete_tipi(){
+    window.location = "../html/rete_tipi.html"
+}
+
+function home(){
+    window.location = "../html/index.html"
+}
+
+function rete_cavi(){
+    window.location = "../html/rete_cavi.html"
+}
+function onde()
+{
+    window.location = "../html/onde.html"
+}
+function back_cavi()
+{
+    window.location = "rete_cavi.html"
+}
+
+function back_tipologie()
+{
+    window.location = "rete_tipi.html"
+}
+
+function cat5()
+{
+    window.location = "cat5.html"
+}
+
+function cat6()
+{
+    window.location = "cat6.html"
+}
+
+function cat7()
+{
+    window.location = "cat7.html"
+}
+
+function fibra()
+{
+    window.location = "fibra.html"
+}
+
+function coassiale()
+{
+    window.location = "coassiale.html"
+}
+
+function pan()
+{
+    window.location = "pan.html"
+}
+
+function lan()
+{
+    window.location = "lan.html"
+}
+
+function man()
+{
+    window.location = "man.html"
+}
+
+function wan()
+{
+    window.location = "wan.html"
+}
+
+function gan()
+{
+    window.location = "gan.html"
+}
+
+const showMoreButton = document.getElementById('showMoreButton');
+const hiddenButtons = document.getElementById('hiddenButtons');
+
+showMoreButton.addEventListener('click', function() {
+  hiddenButtons.style.display = 'block';
+  showMoreButton.style.display = 'none';
+});
+
+
+
